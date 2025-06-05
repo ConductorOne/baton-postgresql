bb9faa4cc3a38f91d47ee04a13b06c7de852a132
MB Burch <mb.burch@conductorone.com>
2025-06-05 09:57:52 -0600

    Update sdk to v0.3.10 and update config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,16 +5,46 @@ import (
 )
 
 var (
-	dsn                 = field.StringField("dsn", field.WithRequired(true), field.WithDescription("The DSN to connect to the database"))
-	schemas             = field.StringSliceField("schemas", field.WithDefaultValue([]string{"public"}), field.WithDescription("The schemas to include in the sync"))
-	includeColumns      = field.BoolField("include-columns", field.WithDescription("Include column privileges when syncing. This can result in large amounts of data"))
-	includeLargeObjects = field.BoolField("include-large-objects", field.WithDescription("Include large objects when syncing. This can result in large amounts of data"))
-	syncAllDatabases    = field.BoolField("sync-all-databases", field.WithDescription("Sync all databases. This can result in large amounts of data"), field.WithDefaultValue(false))
+	dsn = field.StringField(
+		"dsn",
+		field.WithDisplayName("DSN"),
+		field.WithRequired(true),
+		field.WithDescription("The DSN to connect to the database"),
+		field.WithIsSecret(true),
+	)
+	schemas = field.StringSliceField(
+		"schemas",
+		field.WithDisplayName("Schemas"),
+		field.WithDefaultValue([]string{"public"}),
+		field.WithDescription("The schemas to include in the sync"),
+	)
+	includeColumns = field.BoolField(
+		"include-columns",
+		field.WithDisplayName("Include Columns"),
+		field.WithDescription("Include column privileges when syncing. This can result in large amounts of data"),
+	)
+	includeLargeObjects = field.BoolField(
+		"include-large-objects",
+		field.WithDisplayName("Include Large Objects"),
+		field.WithDescription("Include large objects when syncing. This can result in large amounts of data"),
+	)
+	syncAllDatabases = field.BoolField(
+		"sync-all-databases",
+		field.WithDisplayName("Sync All Databases"),
+		field.WithDescription("Sync all databases. This can result in large amounts of data"),
+		field.WithDefaultValue(false),
+	)
 )
 
-var relationships = []field.SchemaFieldRelationship{}
-
 //go:generate go run ./gen
-var Config = field.NewConfiguration([]field.SchemaField{
-	dsn, schemas, includeColumns, includeLargeObjects, syncAllDatabases,
-}, relationships...)
+var Config = field.NewConfiguration(
+	[]field.SchemaField{
+		dsn,
+		schemas,
+		includeColumns,
+		includeLargeObjects,
+		syncAllDatabases,
+	},
+	field.WithConnectorDisplayName("PostgreSQL"),
+	field.WithHelpUrl("/docs/baton/postgresql"),
+)
